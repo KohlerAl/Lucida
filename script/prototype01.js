c5598558e6cfbff0645db443834ea1a8c33678e5
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2021-04-29 22:55:53 +0200

    Add test 1

---
+++ script/prototype01.js
@@ -0,0 +1,32 @@
+"use strict";
+var prototype01;
+(function (prototype01) {
+    let canvas;
+    let ctx;
+    let width;
+    let height;
+    const motionManager = new DeviceMotionAndOrientationManager();
+    motionManager.onRotationRate = onRotationRate;
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        canvas = document.querySelector("canvas");
+        ctx = canvas.getContext("2d");
+        width = window.innerWidth;
+        height = window.innerHeight;
+        canvas.style.width = width + "px";
+        canvas.style.width = height + "px";
+        ctx.beginPath();
+        ctx.strokeStyle = "red";
+        ctx.rect(20, 20, 10, 20);
+        ctx.stroke();
+    }
+    function onRotationRate(_alpha, _beta, _gamma) {
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "white";
+        ctx.rect(0, 0, width, height);
+        ctx.stroke();
+        alert(_alpha + _beta + _gamma);
+    }
+})(prototype01 || (prototype01 = {}));
+//# sourceMappingURL=prototype01.js.map
---
+++ script/prototype01.ts
@@ -0,0 +1,36 @@
+namespace prototype01 {
+    let canvas: HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D;
+    let width: number;
+    let height: number; 
+
+    const motionManager: DeviceMotionAndOrientationManager = new DeviceMotionAndOrientationManager();
+    motionManager.onRotationRate = onRotationRate;
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(): void {
+        canvas = <HTMLCanvasElement>document.querySelector("canvas");
+        ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+        width = window.innerWidth;
+        height = window.innerHeight; 
+        canvas.style.width = width + "px"; 
+        canvas.style.width = height + "px"; 
+
+        ctx.beginPath();
+        ctx.strokeStyle = "red";
+        ctx.rect(20, 20, 10, 20);
+        ctx.stroke();
+
+    }
+
+    function onRotationRate(_alpha: number, _beta: number, _gamma: number): void {
+        ctx.beginPath();
+        ctx.fillStyle = "white"; 
+        ctx.strokeStyle = "white";
+        ctx.rect(0, 0, width, height);
+        ctx.stroke();
+
+        alert(_alpha +  _beta + _gamma); 
+    }
+}
