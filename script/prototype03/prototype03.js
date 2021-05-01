1bc46415202768130482e37f8a94ca57725d1ea4
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2021-05-01 11:49:30 +0200

    Upload prototype 3

---
+++ script/prototype03/prototype03.js
@@ -0,0 +1,71 @@
+"use strict";
+var prototype03;
+(function (prototype03) {
+    //canvas Element and rendering context to draw on canvas
+    let canvas;
+    let ctx;
+    //width and height of the window
+    let width;
+    let height;
+    //The middle-position of the green box
+    let startPos;
+    // Installing a load- and a deviceorientation-Listener on window
+    window.addEventListener("load", handleLoad);
+    window.addEventListener("deviceorientation", handleMove);
+    function handleLoad() {
+        // create device motion/orientation manager
+        const motionManager = new DeviceMotionAndOrientationManager();
+        // create start screen and register device motion/orientation manager
+        const startScreen = new StartScreen("start-screen");
+        startScreen.addResourceManager(motionManager);
+        startScreen.start();
+        //Selecting the canvas and the rendering divs and assigning the values to the prepared variables
+        canvas = document.querySelector("canvas");
+        ctx = canvas.getContext("2d");
+        //To get the correct size of the screen, we select the html-Element and get its width and height
+        let html = document.querySelector("html");
+        width = html.clientWidth;
+        height = html.clientHeight;
+        //To make the canvas as big as the screen, the width and height of the html are applied to it
+        canvas.setAttribute("width", width + "px");
+        canvas.setAttribute("height", height + "px");
+        //Preparing the position of the box. The box should be in the middle, 
+        //so we are dividing the width by two and subtracting half of the width the box will have
+        startPos = (width / 2) - 25;
+        //To prepare the canvas, a white rectangle is drawn on it covering the whole canvas
+        undoCanvas();
+        //Then the box is drawn
+        drawRectangle(startPos);
+    }
+    //Function called when the mobile device is moving
+    function handleMove(_event) {
+        //Check if the value we need is there
+        if (_event.gamma) {
+            //To remove the old rectangle, a white rectangle is drawn covering the whole canvas
+            undoCanvas();
+            //The new position (= movement of device on the y-Axis) is added to the startPosition (middle Position)
+            let newPos = startPos + (_event.gamma * 2);
+            //And the box is drawn
+            drawRectangle(newPos);
+        }
+    }
+    function undoCanvas() {
+        ctx.beginPath();
+        ctx.fillStyle = "#aaaaaa00";
+        ctx.strokeStyle = "aaaaaa00";
+        ctx.rect(0, 0, width, height);
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawRectangle(_startX) {
+        let _startY = height / 2 - 45;
+        ctx.beginPath();
+        ctx.strokeStyle = "lightgreen";
+        ctx.fillStyle = "lightgreen";
+        ctx.rect(_startX, _startY, 50, 50);
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+    }
+})(prototype03 || (prototype03 = {}));
+//# sourceMappingURL=prototype03.js.map
