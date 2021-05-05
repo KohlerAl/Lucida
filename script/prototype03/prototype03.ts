d407a7aadf8b2f11dcebe6c3cee736c7635b1d90
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2021-05-05 22:25:21 +0200

    change position of rocket

--- script/prototype03/prototype03.ts
+++ script/prototype03/prototype03.ts
@@ -96,7 +96,7 @@ namespace prototype03 {
 
     function drawRectangle(_startX: number): void {
         ctxR.clearRect(0, 0, canvasRocket.width, canvasRocket.height);
-        let _startY: number = height / 2 - 45;
+        let _startY: number = height / 2 + 60;
         let rocket: HTMLImageElement = <HTMLImageElement>document.querySelector(".rocket"); 
         ctxR.drawImage(rocket, _startX, _startY, 50, 100);
     }
