f1df9ba0f82b38658d78903e6e2a5d0353b2cc69
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2021-05-19 11:43:43 +0200

    more testing

--- script/prototype04/CanvasObjects/Rocket/Rocket.ts
+++ script/prototype04/CanvasObjects/Rocket/Rocket.ts
@@ -37,9 +37,9 @@ namespace prototype04 {
                 this.position.x = 50;
             }
             else if (this.position.x > width - 50) {
-                this.position.x = width - 50; 
+                this.position.x = width - 80; 
             }
-            if (this.position.x < width / 5 || this.position.x > (width * 0.8)) {
+            if (this.position.x < width / 10 || this.position.x > (width * 0.9)) {
                 this.position.x = this.position.x + _add;
             }
             else {
